f692a440850ecefefc0c200ed98bf35deae9ca08
Gustavo Brenner <gusynched@gmail.com>
2025-06-26 15:38:01 -0300

    Endereço, atualizar endreço, troca de senha, logout

--- foodapp/app/urls.py
+++ foodapp/app/urls.py
@@ -3,7 +3,8 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_view
-from .forms import LoginForm, MyPasswordResetForm
+from django.contrib.auth.views import logout_then_login
+from .forms import LoginForm, MyPasswordChangeForm, MyPasswordResetForm
 urlpatterns = [
     path("",views.home),
     path('about/',views.about, name="about"),
@@ -12,12 +13,16 @@ urlpatterns = [
     path("product-detail/<int:pk>", views.ProductDetail.as_view(), name= "product-detail"),
     path("category-title/<val>", views.CategoryTitle.as_view(), name = "category-title"),
     path('profile/', views.ProfileView.as_view(), name= 'profile'),
-    path('address/', views.ProfileView.as_view(), name= 'address'),
+    path('address/', views.address, name= 'address'),
+    path('updateAddress/<int:pk>', views.updateAdress.as_view(), name= 'updateAddress'),
 
     #login e registro no sitema
     path('registration/', views.CustomerRegistrationView.as_view(), name = "customerregistration"),
     path('accounts/login', auth_view.LoginView.as_view(template_name = "app/login.html", authentication_form= LoginForm), name = "login"),
     path('password-reset/', auth_view.PasswordResetView.as_view(template_name = "app/password_reset.html",form_class = MyPasswordResetForm), name = "password_reset"),
+    path('passwordchange/', auth_view.PasswordChangeView.as_view(template_name = "app/passwordchange.html", form_class = MyPasswordChangeForm, success_url = '/passwordchangedone'), name = 'passwordchange'),
+    path('passwordchangedone/', auth_view.PasswordChangeDoneView.as_view(template_name = 'app/passwordchangedone.html'), name = 'passwordchangedone'),
+    path('logout/', views.UserLogoutView.as_view(http_method_names = ['get', 'post', 'options']), name='logout'),
 
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- foodapp/app/views.py
+++ foodapp/app/views.py
@@ -1,10 +1,12 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from urllib import request
 from django.views import View
-from . models import Product
+from . models import Customer, Product
 from django.db.models import Count
 from django.contrib import messages
+from django.contrib.auth import logout
+from django.contrib.auth.views import LogoutView
 
 
 from . forms import CustomerRegistrationForm, CustomerProfileForm
@@ -55,6 +57,52 @@ class ProfileView(View):
         form  = CustomerProfileForm()
         return render(request, 'app/profile.html', locals())
     def post(self,request):
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid():
+            user = request.user
+            name = form.cleaned_data['name']
+            locality = form.cleaned_data['locality']
+            city = form.cleaned_data['city']
+            mobile = form.cleaned_data['mobile']
+            state = form.cleaned_data['state']
+            zipcode = form.cleaned_data['zipcode']
+
+            reg = Customer(user = user, name=name, locality=locality,mobile = mobile, city=city, state=state, zipcode = zipcode)
+            reg.save()
+            messages.success(request, "Parabéns! Perfil salvo com sucesso")
+        else:
+            messages.warning(request, "Dados inválidos, tente novamente")
         return render(request, 'app/profile.html', locals())
     
+def address(request):
+    add = Customer.objects.filter(user = request.user)
+    return render(request, 'app/address.html', locals())
 
+class updateAdress(View):
+    def get(self,request,pk):
+        add = Customer.objects.get(pk=pk)
+        form=CustomerProfileForm(instance=add)
+        return render(request, 'app/updateAddress.html', locals())
+    def post(self,request,pk):
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid():
+            add = Customer.objects.get(pk=pk)
+            add.name = form.cleaned_data['name']
+            add.locality = form.cleaned_data['locality']
+            add.city = form.cleaned_data['city']
+            add.mobile = form.cleaned_data['mobile']
+            add.state = form.cleaned_data['state']
+            add.zipcode = form.cleaned_data['zipcode']
+            add.save()
+            messages.success(request, "Parabéns! Perfil atualizado com sucesso")
+        else:
+            messages.warning(request, "Tipo de dados inválidos, tente novamente")
+
+
+        return redirect("address")
+    
+class UserLogoutView(LogoutView):
+
+    def get(self, request):
+        logout(request)
+        return redirect('login')
